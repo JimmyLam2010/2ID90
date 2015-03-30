458ecabfae86525d48afdb0e48374f757a316ca6
YimingLin <jimmy201028@gmail.com>
2015-03-30 16:38:56 +0200

    pull

--- assignment4/spellchecker/src/SpellCorrector.java
+++ assignment4/spellchecker/src/SpellCorrector.java
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-import java.util.HashSet;
-
-public class SpellCorrector {
-    final private CorpusReader cr;
-    final private ConfusionMatrixReader cmr;
-    
-    final char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz'".toCharArray();
-    
-    
-    public SpellCorrector(CorpusReader cr, ConfusionMatrixReader cmr) 
-    {
-        this.cr = cr;
-        this.cmr = cmr;
-    }
-    
-    public String correctPhrase(String phrase)
-    {
-        if(phrase == null || phrase.length() == 0)
-        {
-            throw new IllegalArgumentException("phrase must be non-empty.");
-        }
-            
-        String[] words = phrase.split(" ");
-        String finalSuggestion = "";
-        for (String word : words) {
-            if (cr.inVocabulary(word) == false) {
-                getCandidateWords(word);
-            } 
-        } 
-        
-        /** CODE TO BE ADDED **/
-        
-        return finalSuggestion.trim();
-    }
-    
-    public double calculateChannelModelProbability(String suggested, String incorrect) 
-    {
-         /** CODE TO BE ADDED **/
-        
-        return 0.0;
-    }
-         
-      
-    public HashSet<String> getCandidateWords(String word)
-    {
-        HashSet<String> ListOfWords = new HashSet<String>();
-        
-        /** CODE TO BE ADDED **/
-        
-        return cr.inVocabulary(ListOfWords);
-    }          
-=======
 import java.util.HashSet;
 
 public class SpellCorrector {
@@ -109,5 +56,4 @@ public class SpellCorrector {
         }        
         return cr.inVocabulary(ListOfWords);
     }          
->>>>>>> origin/master
 }
